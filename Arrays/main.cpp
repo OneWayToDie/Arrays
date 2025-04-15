34b7a069d42e3f185ec5e04991c7fe7a29d94008
Danila <Danila@DESKTOP-44LP0PV>
2025-04-15 20:16:13 +0300

    Add project files.

---
+++ Arrays/main.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+//#define massive
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	/*const int  n = 5; // элементы массива
+	int arr[n] = { 3, 5, 8}; // инициализированый массив
+	arr[1] = 1024; // второй элемент массива
+	cout << arr[1] << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;*/
+
+
+#ifdef massive
+
+	const int  n = 5;
+	int arr[n];
+	int sum = 0;
+	int min = 0;
+	int max = 0;
+	cout << "¬ведите через enter значени€ массива: "; 
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	min = arr[0];
+	max = arr[0];
+	cout << "вывод массива - ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+		sum = sum+arr[i];
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	cout << endl;
+	cout << "инверси€ массива - ";
+	for (int i = n-1; i >= 0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "сумма элементов массива - " << sum << "\n"; 
+	cout << "среднее арифметическое - " << sum / n << endl;
+	cout << "минимальное значение массива - " << min << endl;
+	cout << "максимальное значение массива - " << max << endl;
+#endif
+}
+
