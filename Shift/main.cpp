d5bfa9c9f53ab391428c3da278765681ad992f0d
Danila <Danila@DESKTOP-44LP0PV>
2025-04-16 21:29:06 +0300

    234

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 
 //#define shift
+#define shift_vpravo
 
 void main()
 {
@@ -29,4 +30,24 @@ void main()
 	}
 #endif
 
+#ifdef shift_vpravo
+	const int  n = 10;
+	int kolichestvo_sdvigov;
+	cout << "Введите количество сдвигов: "; cin >> kolichestvo_sdvigov;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int zero;
+	for (int j = 0; j < kolichestvo_sdvigov; j++)
+	{
+		zero = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			i + 1 < n ? arr[i] = arr[i + 1] : arr[i] = zero;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+	}
+#endif
 }
